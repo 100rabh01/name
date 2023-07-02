6d090e676bf2726e3b1db872d4c77fa9c483f635
Saurabh <saurabhmahapatra007@gmail.com>
2023-07-02 14:39:25 +0530

    add 1solidRect.cpp in 1.Patterns folder

---
+++ 1.Patterns/1solidRect.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define Int unsigned int
+
+int main()
+{
+   int num;
+   cin>>num;
+   for (int i = 0; i < num; ++i)
+   {
+   	cout<<i;
+   }
+  return 0;
+}
